29e9eed61c4cc7ceafdfefbba6d571d29e2e6904
Arma Burton <arma@armaburton.com>
2023-05-24 16:50:14 -0700

    Bob Smith was born on Wednesday, 22, December, 1965

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Packt.Shared;
+
+Person bob = new()
+{
+  Name = "Bob Smith",
+  DateOfBirth = new DateTime(1965, 12, 22)
+};
+
+WriteLine("{0} was born on {1:dddd, d, MMMM, yyy}",
+  arg0: bob.Name,
+  arg1: bob.DateOfBirth
+);
