a57fdcf317fb31f203dfad0893865da71b5614e8
Arma Burton <arma@armaburton.com>
2023-05-25 13:26:17 -0700

    passing parameters

--- Program.cs
+++ Program.cs
@@ -121,4 +121,12 @@ WriteLine(
   arg2: gunny.Instantiated
 );
 
+int a = 10;
+int b = 20;
+int c = 30;
+
+WriteLine($"Before: a = {a}, b = {b}, c = {c}");
+bob.PassingParameters(a, ref b, out c);
+WriteLine($"After: a = {a}, b = {b}, c = {c}");
+
 
