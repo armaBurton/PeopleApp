d37ce32d1945292bc259144c632864292b93596b
Arma Burton <arma@armaburton.com>
2023-05-26 17:28:54 -0700

    families have been generated

--- Program.cs
+++ Program.cs
@@ -185,3 +185,34 @@ WriteLine(
   "{0}: {1} written by {2} has {3:N0} pages.",
   book.Isbn, book.Title, book.Author, book.PageCount
 );
+
+Person lamech = new() { Name = "Lamech" };
+Person adah = new() { Name = "Adah" };
+Person zillah = new() { Name = "Zillah" };
+
+lamech.Marry(adah);
+Person.Marry(zillah, lamech);
+
+WriteLine($"{ lamech.Name } is married to { lamech.Spouse?.Name ?? "nobody" }");
+WriteLine($"{ adah.Name } is married to { adah.Spouse?.Name ?? "nobody" }");
+WriteLine($"{ zillah.Name } is married to { zillah.Spouse?.Name ?? "nobody" }");
+
+Person baby1 = lamech.ProcreateWith(adah);
+baby1.Name = "Jabal";
+WriteLine($"{baby1.Name} was born on {baby1.DateOfBirth}");
+
+Person baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tubalcain";
+
+WriteLine($"{lamech.Name} has {lamech.Children.Count} children.");
+WriteLine($"{adah.Name} has {adah.Children.Count} children.");
+WriteLine($"{zillah.Name} has {zillah.Children.Count} children.");
+
+for (int i = 0; i < lamech.Children.Count; i++){
+  WriteLine(
+    format: "{0}'s child #{1} is named \"{2}\"",
+    arg0: lamech.Name,
+    arg1: i,
+    arg2: lamech[i].Name
+  );
+}
